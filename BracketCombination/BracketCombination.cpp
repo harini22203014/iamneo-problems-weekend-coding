875e9455d46404d70c4911eafd0734f44776f981
harini22203014 <119642200+harini22203014@users.noreply.github.com>
2024-06-09 23:18:02 +0530

    Update BracketCombination.cpp

--- BracketCombination/BracketCombination.cpp
+++ BracketCombination/BracketCombination.cpp
@@ -1,1 +1,25 @@
+// C++ program to find 
+// factorial of given number 
+#include <iostream> 
+using namespace std; 
+
+// Function to find factorial 
+// of given number 
+unsigned int factorial(unsigned int n) 
+{ 
+	if (n == 0 || n == 1) 
+		return 1; 
+	return n * factorial(n - 1); 
+} 
+
+// Driver code 
+int main() 
+{ 
+	int n; 
+	cin>>n;
+	int res=(factorial(n*2))/((factorial(n+1)*factorial(n)));
+	cout<<res;
+	return 0; 
+} 
+
 
